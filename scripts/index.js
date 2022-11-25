c98f2a56d76ad6e1cf928a30462de8cd1c92cf71
chashchinavera <113772051+chashchinavera@users.noreply.github.com>
2022-11-25 14:47:13 +0300

    Обновление файлов

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ const popupElement = document.querySelector('.popup');
 const profileElement = document.querySelector('.profile');
 
 //Buttons
-const elementLikeButtonElement = document.querySelectorAll('.element__like');
+const elementLikeButtonElement = document.querySelector('.element__like');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 const popupOpenButtonElement = profileElement.querySelector('.profile__edit');
 const popupSubmitButtonElement = popupElement.querySelector('.popup__submit');
@@ -14,28 +14,8 @@ const popupInputNameName = popupElement.querySelector('.popup__input_name_name')
 const popupInputNameStatus = popupElement.querySelector('.popup__input_name_status');
 
 //Functions
-const activeElementLike0 = function () {
-    elementLikeButtonElement[0].classList.toggle('element__like_active');
-}
-
-const activeElementLike1 = function () {
-    elementLikeButtonElement[1].classList.toggle('element__like_active');
-}
-
-const activeElementLike2 = function () {
-    elementLikeButtonElement[2].classList.toggle('element__like_active');
-}
-
-const activeElementLike3 = function () {
-    elementLikeButtonElement[3].classList.toggle('element__like_active');
-}
-
-const activeElementLike4 = function () {
-    elementLikeButtonElement[4].classList.toggle('element__like_active');
-}
-
-const activeElementLike5 = function () {
-    elementLikeButtonElement[5].classList.toggle('element__like_active');
+const activeElementLike = function () {
+    elementLikeButtonElement.classList.toggle('element__like_active');
 }
 
 const openPopup = function () {
@@ -58,10 +38,4 @@ const submitPopup = function () {
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 popupSubmitButtonElement.addEventListener('click', submitPopup);
-elementLikeButtonElement[0].addEventListener('click', activeElementLike0);
-elementLikeButtonElement[1].addEventListener('click', activeElementLike1);
-elementLikeButtonElement[2].addEventListener('click', activeElementLike2);
-elementLikeButtonElement[3].addEventListener('click', activeElementLike3);
-elementLikeButtonElement[4].addEventListener('click', activeElementLike4);
-elementLikeButtonElement[5].addEventListener('click', activeElementLike5);
-
+elementLikeButtonElement.addEventListener('click', activeElementLike);
