c9e0191cbae2b30013c66e4ced4d84283b05aafc
chashchinavera <113772051+chashchinavera@users.noreply.github.com>
2022-11-27 16:38:54 +0300

    Обновление файлов

--- scripts/index.js
+++ scripts/index.js
@@ -3,39 +3,34 @@ const popupElement = document.querySelector('.popup');
 const profileElement = document.querySelector('.profile');
 
 //Buttons
-const elementLikeButtonElement = document.querySelector('.element__like');
-const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 const popupOpenButtonElement = profileElement.querySelector('.profile__edit');
+const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 const popupSubmitButtonElement = popupElement.querySelector('.popup__submit');
 
 const profileName = profileElement.querySelector('.profile__name');
-const profileStatus = profileElement.querySelector('.profile__status')
-const popupInputNameName = popupElement.querySelector('.popup__input_name_name');
-const popupInputNameStatus = popupElement.querySelector('.popup__input_name_status');
+const profileStatus = profileElement.querySelector('.profile__status');
+const popupInputInfoName = popupElement.querySelector('.popup__input_info_name');
+const popupInputInfoStatus = popupElement.querySelector('.popup__input_info_status');
 
 //Functions
-const activeElementLike = function () {
-    elementLikeButtonElement.classList.toggle('element__like_active');
-}
-
 const openPopup = function () {
+    popupInputInfoName.value = profileName.textContent;
+    popupInputInfoStatus.value = profileStatus.textContent;
     popupElement.classList.add('popup_opened');
-    popupInputNameName.value = profileName.textContent;
-    popupInputNameStatus.value = profileStatus.textContent;
 }
 
 const closePopup = function () {
     popupElement.classList.remove('popup_opened');
 }
 
-const submitPopup = function () {
-    profileName.textContent = popupInputNameName.value;
-    profileStatus.textContent = popupInputNameStatus.value;
-    popupElement.classList.remove('popup_opened');
+const submitPopup = function (evt) {
+    evt.preventDefault();
+    profileName.textContent = popupInputInfoName.value;
+    profileStatus.textContent = popupInputInfoStatus.value;
+    closePopup();
 }
 
 //AddEventListener
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
-popupSubmitButtonElement.addEventListener('click', submitPopup);
-elementLikeButtonElement.addEventListener('click', activeElementLike);
+popupSubmitButtonElement.addEventListener('submit', submitPopup);
